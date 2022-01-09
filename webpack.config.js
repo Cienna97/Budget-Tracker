02ca2cb5c54eda433b267120a222752583bb0ec6
Cienna97 <cmortenson97@gmail.com>
2022-01-09 00:28:37 -0700

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,10 @@ const path = require("path");
 
 
 
-module.exports = {};
+module.exports = {entry: './public/js/index.js',
+output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: 'development'
+};
