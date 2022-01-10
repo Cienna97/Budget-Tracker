df33fe0b8a667e5c30f966049c5898de3b7c8070
Cienna97 <cmortenson97@gmail.com>
2022-01-09 17:26:39 -0700

    adding to idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,13 +1,13 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called '' and set it to version 1
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open("budget", 1);
 
 // event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 //request.onupgradeneeded = function(event) {
      // save a reference to the database
-    request.onupgradeneeded = ({ target }) => {
-        let db = target.result;
+    request.onupgradeneeded = function(event) {
+        const db = event.target.result;
         db.createObjectStore("pending",
         { autoIncrement: true });
     };
@@ -15,7 +15,7 @@ const request = indexedDB.open('budget', 1);
     request.onsuccess = function(event) {
         // when db is successfully created with its object store
    db = event.target.result;
-    }
+    
     //const db = event.target.result;
     // create an object store (table) called '
 
@@ -24,6 +24,25 @@ const request = indexedDB.open('budget', 1);
         checkDatabase();
     }
 };
+
+request.onerror = function(event) {
+    console.log("Error!" + event.target.errorCode);
+};
+
+//This function will be executed if no internet connection
+function saveRecord(record) {
+    //open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    //access the object store for "pending"
+    const store = transaction.createObjectStore("pending");
+
+    //add record to your store with add method
+    store.add(record);
+}
+
+
+
 
 
 
