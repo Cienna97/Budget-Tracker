5c64ec32c8bba8914bab464770f38b681a64445c
Cienna97 <cmortenson97@gmail.com>
2022-01-13 18:29:07 -0700

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_PREFIX = 'my-site-cache-';  
+const APP_PREFIX = 'Budget-Tracker';  
 const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = "data-cache-" + VERSION;
